2febffd04d3c02eb24da3d6411802d579ed2bbee
woynert <whayner.porras.2020@upb.edu.co>
2023-12-08 00:00:30 -0500

    feat(api): return hostname as well

--- api/main.go
+++ api/main.go
@@ -3,29 +3,34 @@ package main
 import (
 	"net/http"
 	"os/exec"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func ping (ctx *gin.Context) {
-    ctx.Status(http.StatusOK)
+func ping(ctx *gin.Context) {
+	ctx.Status(http.StatusOK)
+	hostname, err := exec.Command("hostname").Output()
+	if err != nil {
+		ctx.Status(http.StatusInternalServerError)
+	}
+	ctx.JSON(http.StatusOK, gin.H{"hostname": strings.TrimSpace(string(hostname))})
 }
 
-func shutdown (ctx *gin.Context) {
-    cmd := exec.Command("sh", "-c", "touch /tmp/wemo3000-`date +\"%T\"`")
-    err := cmd.Run()
-    if (err != nil) {
-        ctx.Status(http.StatusInternalServerError)
-    }
-    ctx.Status(http.StatusOK)
+func shutdown(ctx *gin.Context) {
+	cmd := exec.Command("sh", "-c", "touch /tmp/wemo3000-`date +\"%T\"`")
+	err := cmd.Run()
+	if err != nil {
+		ctx.Status(http.StatusInternalServerError)
+	}
+	ctx.Status(http.StatusOK)
 }
 
-func main()  {
-    engine := gin.Default()
+func main() {
+	engine := gin.Default()
 
-    // discover service / check if device is ON
+	// discover service / check if device is ON
 	engine.GET("/ping", ping)
 	engine.GET("/shutdown", shutdown)
-
-    engine.Run(":2333")
+	engine.Run(":2333")
 }
